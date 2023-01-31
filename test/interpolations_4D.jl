5991e2cbb3cfaf2b2e1e09cf273a0a99ce841a18
anthony-meza <64243783+anthony-meza@users.noreply.github.com>
2023-01-31 12:55:49 -0500

    Update interpolations_4D.jl
    Functions added to GH19 are: read4Dfield and random_observations. Random_observations takes a random location (x-y-z) and returns the time series

--- test/interpolations_4D.jl
+++ test/interpolations_4D.jl
@@ -5,12 +5,11 @@ using Test
 using DrWatson
 using NetCDF, TMI
 
-fname = datadir("Theta_EQ-0015.nc")
 A, Alu, γ, TMIfile, L, B = config_from_nc("modern_180x90x33_GH11_GH12")
 
 # gets puts netcdf in alist of type "Field"
 # Field type could probably be extended to 4D to avoid repitition. 
-@time cs = read4Dfield(fname, "theta", γ)
+@time cs = read4Dfield(datadir("Theta_EQ-0015.nc"), "theta", γ)
 
 # samples a random (or set of random) points 
 N = 2
