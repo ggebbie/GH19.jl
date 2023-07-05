bcdb180ae83be55a1f3bca5d427e481e7a69128e
G Jake Gebbie <ggebbie@whoi.edu>
2023-07-05 11:24:25 -0400

    extract timeseries at cartesian and any location

--- src/GH19.jl
+++ src/GH19.jl
@@ -1,8 +1,9 @@
 module GH19
 
-using Downloads
+using Downloads, TMI, NCDatasets
 
-export explist, pkgdir, datadir, srcdir, download, download_all
+export explist, pkgdir, datadir, srcdir, download, download_all, TMIversion,
+    extract_timeseries
 
 import Downloads: download
 
@@ -26,6 +27,8 @@ rooturl() = "https://www.ncei.noaa.gov/pub/data/paleo/gcmoutput/gebbie2019"
 
 rooturl(args...) = joinpath(rooturl(), args...)
 
+TMIversion() = "modern_180x90x33_GH11_GH12" # all GH19 output uses this TMI version, no underscore = function
+
 """
     function download(experiment::String;anomaly=false)
 
@@ -35,7 +38,7 @@ rooturl(args...) = joinpath(rooturl(), args...)
 # Output
 - `outputfile`: name of loaded file, found in the `datadir()` directory
 """
-function download(experiment::String,anomaly=false)
+function download(experiment::String,anomaly=false;force = false)
     
     if anomaly
         filename = "Theta_anom_"*experiment*".nc"
@@ -45,10 +48,13 @@ function download(experiment::String,anomaly=false)
     infile = rooturl(filename)
     outfile = datadir(filename)
 
-    !isdir(datadir()) && mkpath(datadir()) 
-
-    Downloads.download(infile,outfile,verbose=true)
-
+    if !isfile(outfile) || force == true
+        !isdir(datadir()) && mkpath(datadir()) 
+        Downloads.download(infile,outfile,verbose=true)
+    else
+        println("File already downloaded; use `force=true` to re-downloade")
+    end
+    return outfile
 end
 
 """
@@ -69,4 +75,84 @@ function download_all()
     return outputfiles
 end
 
+function readtracer_snapshot(file::String,tracername::Union{String,Symbol},time_index::Integer)
+    ds = Dataset(file,"r")
+    v = ds[tracername]
+    # load one snapshot, reverse zyx order to xyz order
+    tracer = permutedims(v[time_index,:,:,:],(3,2,1))
+    # load an attribute
+    units = v.attrib["units"]
+    longname = v.attrib["long_name"]
+    close(ds)
+    return tracer,units,longname
 end
+
+"""
+    function readfield(file,tracername,γ)
+    Read a tracer field from NetCDF but return it 
+    as a Field.
+
+    Use NCDatasets so that Unicode is correct
+
+# Arguments
+- `file`: TMI NetCDF file name
+- `tracername`: name of tracer
+- `γ::Grid`, TMI grid specification
+# Output
+- `c`::Field
+"""
+function readfield_snapshot(file,tracername,time_index::Integer,γ::TMI.Grid) 
+
+    # The mode "r" stands for read-only. The mode "r" is the default mode and the parameter can be omitted.
+    tracer, units, longname = readtracer_snapshot(file,tracername,time_index)
+    #TMI.checkgrid!(tracer,γ.wet)
+    c = Field(tracer,γ,Symbol(tracername),longname,units)
+
+    return c
+end
+
+"""
+function extract_timeseries(
+
+    extract a timeseries at a Cartesian point, i,j,k
+
+    return tuple with tracer timeseries θ at times t
+    i.e., (θ,t)
+"""
+function extract_timeseries(file,tracername,i,j,k)
+        θijk = NCDataset(file)[tracername][:,k,j,i]
+        tijk = NCDataset(file)["year"][:]
+    return θijk, tijk
+end
+
+"""
+    function extract_timeseries(file,tracername,locs,γ)
+
+    extract timeseries at locs. Requires TMI grid, γ.
+
+    readfield_snapshot is very slow.
+
+    Also very slow to recompute interpolation factors each time.
+
+    Need to refactor.
+"""
+function extract_timeseries(file,tracername,locs,γ)
+    t = NCDataset(file)["year"][:]
+    nt = length(t)
+    nlocs = length(locs)
+    θ = Vector{Vector{Float64}}(undef,nt) #NCDataset(file)[tracername][:,k,j,i]
+
+    for time_index in 1:nt
+        println(time_index)
+        c = GH19.readfield_snapshot(file,tracername,time_index,γ)
+        θ[time_index] =  observe(c,locs,γ)
+    end
+    
+    return θ, t
+end
+
+
+
+
+end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -16,7 +16,8 @@ using Test
     end
 
     @testset "download one" begin
-        
+
+        using TMI
         experimentlist = explist()
         expno = rand(1:length(experimentlist))
         exp = experimentlist[1]
@@ -26,12 +27,29 @@ using Test
         @test isfile(filename)
         
         ds = NCDataset(filename)
+
+        # extract a timeseries at a Cartesian point
+        i = 85; j = 35; k = 10;
+        θijk, tijk = extract_timeseries(filename,"theta",i,j,k)
+
+        @test length(θijk) == length(tijk)
+
+        # extract a timeseries at any point
+        # 1. Make a field at index tt
+        γ = TMI.Grid(TMI.download_ncfile(TMIversion()))
+        θt,tt = extract_timeseries(filename,"theta",locs,γ)
+
+        # time_index = 1
+        # c = GH19.readfield_snapshot(filename,tracername,time_index,γ)
+        # locs = [(-30.0,45.0,100.0),(180.0,45.0,100.0)]
+        # cobs =  observe(c,locs,γ)
+
     end
 
-    @testset "compare to TMI grid" begin
-        using TMI
+    # @testset "compare to TMI grid" begin
+    #     using TMI
         
 
-    end
+    # end
     
 end
